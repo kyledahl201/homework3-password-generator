0af8fd650d9fcbb272f24181869e70076b01d3a8
Kyle Dahl <kyle.dahl201@gmail.com>
2020-05-17 23:33:43 -0500

    password input variables

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// write variable arrays for password inputs
+
+//array of lower case letters
+var lowerCase = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+var upperCase = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' 
+]
+var specialCharacters [
+  '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=' 
+]
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
